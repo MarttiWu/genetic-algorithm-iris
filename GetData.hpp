7e33af08ef0494f74cc3801ef12715057d97b74f
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-09-04 08:59:31 +0800

    Create GetData.hpp

---
+++ GetData.hpp
@@ -0,0 +1,23 @@
+//
+//  Ackley.hpp
+//
+//  Created by WU,MENG-TING on 2020/8/14.
+//  Copyright © 2020 WU,MENG-TING. All rights reserved.
+//
+
+#ifndef GetData_hpp
+#define GetData_hpp
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+//namespace Data{
+vector<vector<double> > GetIrisData();
+//}
+
+
+#endif /* GetData_hpp */
