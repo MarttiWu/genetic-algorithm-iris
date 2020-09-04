fdb13979d1884cc441bcdad01a8399c5759c7ab8
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-09-04 09:07:23 +0800

    Update GA.hpp

--- GA.hpp
+++ GA.hpp
@@ -13,7 +13,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstdlib>
-//#include <memory.h>
 #include <ctime>
 #include <cmath>
 #include <algorithm>
@@ -32,27 +31,15 @@ public:
     double total_SSE(vector<double> str,vector<vector<double> > centroids);
     vector<vector<double> > Cal_centroids(vector<double> str);
     void Cal_Fitness();
-    
     double FitnessFunction(vector<double> sol);
     void FindBest();
     void UpdateBest();
     void Cluster();
-    /*
-    int Sum(int *str);
-    int get_max(){return Max;}
-    int find_best_index();
-     */
-    ////////
     vector<double> FitnessFunction();
     vector<vector<double> > RouletteWheelSelection();
     vector<vector<double> > TournamentSelection();
-    
-    
-    
     vector<vector<double> > Crossover(vector<vector<double> > p);
     vector<vector<double> > Mutation(vector<vector<double> > p);
-    
-    
     double get_record_value(int iter){return record_value[iter];}
     vector<double> get_record_str(int iter){return record_str[iter];}
     
@@ -61,10 +48,6 @@ private:
     int digits;
     int pop;
     int dim;
-    //int Max;
-    //vector<vector<double> > centroid;
-    //vector<double> centroid_value;
-    
     vector<double> best_str; //temp
     double best_value;
     double Pm;
@@ -72,14 +55,10 @@ private:
     vector<vector<double> > Pop_str;
     vector<double> Pop_value;
     vector<double> Prob;//for roulette wheel selection
-    //int *f;
     vector<vector<double> > data;
-    //int tnt_count;
     vector<double> v;
     vector<vector<double> > record_str;
     vector<double> record_value;
-    //static int count;
 };
-//int count=0;
 
 #endif /* GA_hpp */
